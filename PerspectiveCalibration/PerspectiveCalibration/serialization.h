84c323a0c5b38dcfb5ef933a65775a400a9f5b47
PiatekBartosz <bartekpiatek123@gmail.com>
2023-09-10 12:29:29 +0200

    PerspectiveCalibration: deleted unecessary class

--- PerspectiveCalibration/PerspectiveCalibration/serialization.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <boost/archive/text_oarchive.hpp>
-#include <boost/archive/text_iarchive.hpp>
-
-
-void serializeCvMat(cv::Mat inputMat, std::string outputFileName);
-void deserializeCvMat(cv::Mat outputMat, std::string inputFileName);
-
